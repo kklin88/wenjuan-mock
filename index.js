059603109d59a5589182e1a35ef206c6e2f5839f
kklin88 <linkui21@gmail.com>
2024-08-25 14:23:06 +0800

    feat: Server獲取前端請求url的參數

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const app = new Koa();
 const router = new Router();
 // 註冊mock路由
 
-async function getRes(fn) {
+async function getRes(fn,ctx) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const res = fn();
+      const res = fn(ctx);
       resolve(res)
     },1000);
   });
 }
+// 註冊mock路由
 mockList.forEach((item) => {
   const { url, method, response } = item;
   router[method](url, async (ctx) => {
-    const res = await getRes(response);//模擬網絡請求的加載狀態
+    const res = await getRes(response,ctx);//模擬網絡請求的加載狀態
     ctx.body = res;
   });
   //   router.get
--- mock/question.js
+++ mock/question.js
@@ -1,10 +1,26 @@
 const Mock = require("mockjs");
 const Random = Mock.Random;
-
+const getQuestionList = require("./data/getQuestion");
 module.exports = [
   {
-    url: "/api/question/:id", // 獲取單個問卷
+    url: "/api/question/:id", // 獲取單個問卷信息
     method: "get",
+    response() {
+      return {
+        errno: 0,
+        data: {
+          id: Random.id(),
+          title: Random.ctitle(),
+        },
+        // errno:1001,
+        // msg:'錯誤測試'
+      };
+    },
+  },
+  {
+    // 創建問卷
+    url: "/api/question",
+    method: "post",
     response() {
       return {
         errno: 0,
@@ -16,14 +32,27 @@ module.exports = [
     },
   },
   {
+    // 獲取（查詢）問卷列表
     url: "/api/question",
-    method: "post",
-    response() {
+    method: "get",
+    response(ctx) {
+      // 服務端根據前端傳回的url決定需要傳回的數據
+      //   獲取前端輸入的url
+      const { url = "", query = {} } = ctx;
+      console.log("url:", url);
+
+      const isStar = url.indexOf("isStar=true") >= 0;
+      const isDeleted = url.indexOf("isDeleted=true") >= 0;
+      console.log(isStar, isDeleted);
+      console.log("ctx query", query);
+      const pageSize = parseInt(query.pageSize) ||10;
+    //   const page = parseInt(query.page);
+
       return {
         errno: 0,
         data: {
-          id: Random.id(),
-          title: Random.ctitle(),
+          list: getQuestionList({len:pageSize, isStar, isDeleted }), //當前頁
+          total: 100, //總數，用於分頁
         },
       };
     },
